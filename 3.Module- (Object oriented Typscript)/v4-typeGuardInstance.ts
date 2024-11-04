81ce2adfb1cde9238c2abc5c53a1149db220f4cb
shahidullah177 <mdshahidsumon177@gmail.com>
2024-11-04 12:30:20 +0600

    Practice going on

---
+++ 3.Module- (Object oriented Typscript)/v4-typeGuardInstance.ts
@@ -0,0 +1,33 @@
+{
+    //instance of guard
+
+class Animal {
+    name: string;
+    species: string;
+
+    constructor(name: string, species: string){
+        this.name = name;
+        this.species = species;
+    }
+
+
+    makeSound(){
+        console.log('I am making sound');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //
+}
