d0e33e96c034587c4e19ada24b5a05b28214004b
shahidullah177 <mdshahidsumon177@gmail.com>
2024-11-05 09:36:53 +0600

    typescript basic

---
+++ 1.Module- (Basics of Typescript)/v4-basicDataTypes.ts
@@ -0,0 +1,16 @@
+//string
+const me: string ="shahidullah";
+//number
+const age: number = 1233;
+//bolean
+const isValid: boolean = true;
+//null
+const seeMore:null = null;
+//undefined
+const fatherName: undefined = undefined;
+
+//array
+const friends:string[] =  ["Bizoy", "hafiz"]
+const alocate:number[] = [1,3,4,5]
+//tuple
+const devs:[number, string] = [12, 'Rakib']
--- 3.Module- (Object oriented Typscript)/v5-accessModifiers.ts
+++ 3.Module- (Object oriented Typscript)/v5-accessModifiers.ts
@@ -1,23 +1,47 @@
 {
     //
+    class BankAccount {
+        public readonly id: number;
+        public name: string;
+        private balance: number;
 
+        constructor(id: number, name: string, balance: number) {
+            this.id = id;
+            this.name = name;
+            this.balance = balance;
+        }
 
+        addDeposit (amount: number){
+            this.balance = this.balance + amount;
+        }
 
+        getBalance (){
+            return this.balance
+        }
 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const goribManusherAccount = new BankAccount(222, "Mr. Fanus", 120);
+    // goribManusherAccount.balance = 1223
+    goribManusherAccount.addDeposit(500)
+    const currentBalance = goribManusherAccount.getBalance()
+    console.log(currentBalance, "============");
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     //
 }
