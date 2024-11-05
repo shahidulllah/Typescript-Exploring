4b23e05079cf9c684ae0b66098201bbe4a480611
shahidullah177 <mdshahidsumon177@gmail.com>
2024-11-05 09:50:35 +0600

    object, optional and literal types

---
+++ 1.Module- (Basics of Typescript)/v5-object,optional,literalTypes.ts
@@ -0,0 +1,14 @@
+//Reference Type ====> Object
+const user:{
+    company: 'This is literal types, not changable and it is totaly fixed'  //===> Literal Type
+    firstName: string;
+    middleName?: string;  //===>Optional Type==?
+    lastName: string;
+    isMarried: boolean;
+} = {
+    company: "This is literal types, not changable and it is totaly fixed",
+    firstName: "Mezba",
+    lastName: "Sili",
+    isMarried: true
+}
+
